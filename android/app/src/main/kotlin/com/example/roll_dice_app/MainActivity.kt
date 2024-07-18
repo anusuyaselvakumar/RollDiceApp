9c68b53f72d48a039ee2fc9a27fc7a92ec3dfc91
anusuyaselvakumar <anusuyaselvakumar2003@gmail.com>
2024-07-18 21:11:31 +0530

    commit

--- android/app/src/main/kotlin/com/example/roll_dice_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/roll_dice_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.diceroll_app
+package com.example.roll_dice_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
-import 'package:flutter/material.dart';
 import 'package:diceroll_app/gradient_color.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       home: Scaffold(
         body: GradiantColor([
           Color.fromARGB(255, 129, 144, 228),
           Color.fromARGB(255, 54, 54, 232)
         ]),
       ),
-    ),
-  );
+    );
+  }
 }
